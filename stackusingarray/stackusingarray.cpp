48b837b35790675496ebed3b34a30eb20a12ba73
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-06-16 15:31:55 +0700

    jika array kosong maka pop tidak bisa dilakukan

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -29,7 +29,7 @@ public:
 
     void pop() {
         if (empty()) { // step 1
-            cout << "Stack is empty. Cannot pop." << endl;     //1.a
+            cout << "Stack is empty. Cannot pop." << endl;      //1.a
             return;   //1.b
         }
 
